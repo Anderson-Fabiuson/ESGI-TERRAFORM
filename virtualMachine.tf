70baa4427cb23125c6c77fe4a0df3fba427964ac
Anderson-Fabiuson <vandersonnahum@gmail.com>
2020-05-31 01:37:59 +0200

    Ajout disk manager

--- virtualMachine.tf
+++ virtualMachine.tf
@@ -1,38 +1,38 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "${var.resource_group}"
-  location = "${var.location}"
+resource "azurerm_resource_group" "RGOne" {
+  name     = "RGOne"
+  location = "East Europe"
 
   tags {
     environment = "${var.env_name}"
   }
 }
 
-resource "azurerm_virtual_network" "vnet" {
+resource "azurerm_virtual_network" "VirtualNetwork" {
   name                = "${var.project_name}vnet"
   location            = "${var.location}"
-  address_space       = ["10.0.0.0/16"]
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+  address_space       = ["192.168.0.0/16"]
+  resource_group_name = "${azurerm_resource_group.RGOne.name}"
 
   tags {
     environment = "${var.env_name}"
   }
 }
 
-resource "azurerm_subnet" "subnet" {
+resource "azurerm_subnet" "Subnet1" {
   name                 = "${var.project_name}subnet"
-  virtual_network_name = "${azurerm_virtual_network.vnet.name}"
-  resource_group_name  = "${azurerm_resource_group.rg.name}"
-  address_prefix       = "10.0.10.0/24"
+  virtual_network_name = "${azurerm_virtual_network.VirtualNetwork.name}"
+  resource_group_name  = "${azurerm_resource_group.RGOne.name}"
+  address_prefixes       = ["192.168.1.0/24"]
 }
 
-resource "azurerm_network_interface" "nic" {
+resource "azurerm_network_interface" "NetworkInterface-nic" {
   name                = "${var.project_name}nic"
   location            = "${var.location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+  resource_group_name = "${azurerm_resource_group.RGOne.name}"
 
   ip_configuration {
     name                          = "${var.project_name}ipconfig"
-    subnet_id                     = "${azurerm_subnet.subnet.id}"
+    subnet_id                     = "${azurerm_subnet.Subnet1.id}"
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = "${azurerm_public_ip.pip.id}"
   }
@@ -41,19 +41,19 @@ resource "azurerm_network_interface" "nic" {
 resource "azurerm_public_ip" "pip" {
   name                         = "${var.project_name}-ip"
   location                     = "${var.location}"
-  resource_group_name          = "${azurerm_resource_group.rg.name}"
+  resource_group_name          = "${azurerm_resource_group.RGOne.name}"
   public_ip_address_allocation = "Dynamic"
-  domain_name_label            = "domterraform"
+  domain_name_label            = "esgiterraform"
 
   tags {
-    environment = "Terraform Meetup"
+    environment = "Terraform Meetup ESGI"
   }
 }
 
 resource "azurerm_storage_account" "stor" {
-  name                     = "${var.project_name}stor"
+  name                     = "${var.project_name}-stor"
   location                 = "${var.location}"
-  resource_group_name      = "${azurerm_resource_group.rg.name}"
+  resource_group_name      = "${azurerm_resource_group.RGone.name}"
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
@@ -62,10 +62,10 @@ resource "azurerm_storage_account" "stor" {
   }
 }
 
-resource "azurerm_managed_disk" "datadisk" {
-  name                 = "${var.project_name}-datadisk"
+resource "azurerm_managed_disk" "ManagedDisk" {
+  name                 = "${var.project_name}-ManagedDisk"
   location             = "${var.location}"
-  resource_group_name  = "${azurerm_resource_group.rg.name}"
+  resource_group_name  = "${azurerm_resource_group.RGone.name}"
   storage_account_type = "Standard_LRS"
   create_option        = "Empty"
   disk_size_gb         = "1023"
@@ -75,17 +75,17 @@ resource "azurerm_managed_disk" "datadisk" {
   }
 }
 
-resource "azurerm_virtual_machine" "vm" {
+resource "azurerm_virtual_machine" "CentOS" {
   name                  = "${var.project_name}vm"
   location              = "${var.location}"
-  resource_group_name   = "${azurerm_resource_group.rg.name}"
+  resource_group_name   = "${azurerm_resource_group.RGOne.name}"
   vm_size               = "Standard_DS1_v2"
-  network_interface_ids = ["${azurerm_network_interface.nic.id}"]
+  network_interface_ids = ["${azurerm_network_interface.NetworkInterface-nic.id}"]
 
   storage_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    offer     = "CentOS7"
+    sku       = "7"
     version   = "latest"
   }
 
@@ -98,7 +98,7 @@ resource "azurerm_virtual_machine" "vm" {
 
   storage_data_disk {
     name              = "${var.project_name}-datadisk"
-    managed_disk_id   = "${azurerm_managed_disk.datadisk.id}"
+    managed_disk_id   = "${azurerm_managed_disk.ManagedDisk.id}"
     managed_disk_type = "Standard_LRS"
     disk_size_gb      = "1023"
     create_option     = "Attach"
